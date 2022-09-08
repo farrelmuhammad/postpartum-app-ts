703522a05cb8478395617f6b645f7038c87bccc2
Farrel Muhammad <81765631+farrelmuhammad@users.noreply.github.com>
2022-09-08 23:07:04 +0700

    Update for CRUD symptoms and authentication

--- .history/src/main_20220908211319.ts
+++ .history/src/main_20220908211319.ts
@@ -1,13 +1,11 @@
-/* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TransformInterceptor())
+  // app.useGlobalInterceptors(new TransformInterceptor())
   const port = 5000;
   await app.listen(port);
 }
